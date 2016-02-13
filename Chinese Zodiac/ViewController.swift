1370a472702dce618c55e68aa64aec8052cc3418
Yalin <mayalin2012@gmail.com>
2016-02-14 07:18:21 +0800

    Chinese Zodiac
     List Chinese zodiac in table view

--- Chinese Zodiac/ViewController.swift
+++ Chinese Zodiac/ViewController.swift
@@ -6,20 +6,47 @@
 //  Copyright © 2016 yalin.ma. All rights reserved.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    let cellIdentifier = "Cell"
+    @IBOutlet weak var tableView: UITableView!
+    //var data = ["🐭 Rat", "🐂 Ox", "🐯 Tiger", "🐰 Rabbit", "🐲 Dragon","🐍 Snake", "🐎 Horse", "🐑 Sheep", "🐒 Monkey", "🐔 Rooster", "🐶 Dog", "🐷 Pig"]
+    var data = ["🐭 鼠", "🐂 牛", "🐯 虎", "🐰 兔", "🐲 龙","🐍 蛇", "🐎 马", "🐑 羊", "🐒 猴", "🐔 鸡", "🐶 狗", "🐷 猪"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView)
+        -> Int {
+            return 1
     }
-
-
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return 12
+    }
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            var cell : UITableViewCell! =
+            tableView.dequeueReusableCellWithIdentifier(cellIdentifier)
+            
+            
+            if cell == nil {
+                cell = UITableViewCell(
+                    style:.Default, reuseIdentifier:cellIdentifier)
+                cell.textLabel!.textColor = UIColor.darkGrayColor()
+                cell.textLabel!.highlightedTextColor = UIColor.brownColor()
+            }
+            
+            
+            cell.textLabel!.text = data[indexPath.row]
+            return cell
+    }
 }
-
